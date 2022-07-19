fe9bd1e263e114b8bdbafb78be33e1e3d6679432
DESKTOP-11USOID\acer <abkkd5524@gmail.com>
2022-07-19 18:32:00 +0800

    finsh mergeSort and addEventListener to sort click

--- app.js
+++ app.js
@@ -94,60 +94,133 @@ button.addEventListener("click", (e) => {
   //跑完整個事件後最後input[type =text]的格子變回空格
   inputText.value = "";
 });
-//delete localstroage and lord data
-let myList = localStorage.getItem("list");
-if (myList !== null) {
-  let myListArray = JSON.parse(myList);
-  myListArray.forEach((item) => {
-    //create a todo
-    let todo = document.createElement("div");
-    todo.classList.add("todo");
-    let text = document.createElement("p");
-    text.classList.add("todo-text");
-    text.innerHTML = item.todoText;
-    let time = document.createElement("p");
-    time.classList.add("todo-time");
-    time.innerText = item.todoMonth + "/" + item.todoDay;
-    todo.appendChild(text);
-    todo.appendChild(time);
 
-    //create trashcan and delete
-    let completeButton = document.createElement("button");
-    completeButton.innerHTML = "<i class='bx bx-check'></i>";
-    completeButton.classList.add("complete");
+//開網站時會顯示存在localstorage裡的資料
+lordData();
+//lord data and delete localstroage
+function lordData() {
+  let myList = localStorage.getItem("list");
+  if (myList !== null) {
+    let myListArray = JSON.parse(myList);
+    myListArray.forEach((item) => {
+      //create a todo
+      let todo = document.createElement("div");
+      todo.classList.add("todo");
+      let text = document.createElement("p");
+      text.classList.add("todo-text");
+      text.innerHTML = item.todoText;
+      let time = document.createElement("p");
+      time.classList.add("todo-time");
+      time.innerText = item.todoMonth + "/" + item.todoDay;
+      todo.appendChild(text);
+      todo.appendChild(time);
 
-    //add complete style
-    completeButton.addEventListener("click", () => {
-      todo.classList.toggle("desh");
-    });
-    let trashCan = document.createElement("button");
-    trashCan.innerHTML = "<i class='bx bx-trash'></i>";
-    trashCan.classList.add("trashCan");
+      //create trashcan and delete
+      let completeButton = document.createElement("button");
+      completeButton.innerHTML = "<i class='bx bx-check'></i>";
+      completeButton.classList.add("complete");
 
-    //add remove to delete
-    trashCan.addEventListener("click", (e) => {
-      let todoItem = e.target.parentElement;
-      //啟動縮小的style，並移除
-      todo.addEventListener("animationend", () => {
-        let text = todoItem.children[0].innerText;
-        let myListArray = JSON.parse(localStorage.getItem("list"));
-        myListArray.forEach((item, index) => {
-          if (item.todoText == text) {
-            myListArray.splice(index, 1);
-            localStorage.setItem("list", JSON.stringify(myListArray));
-          }
-        });
-        todoItem.remove();
+      //add complete style
+      completeButton.addEventListener("click", () => {
+        todo.classList.toggle("desh");
       });
-      //增加縮小的style
-      todo.style.animation = "scaleDown 0.3s forwards";
-    });
-    todo.appendChild(completeButton);
-    todo.appendChild(trashCan);
+      let trashCan = document.createElement("button");
+      trashCan.innerHTML = "<i class='bx bx-trash'></i>";
+      trashCan.classList.add("trashCan");
 
-    section.appendChild(todo);
-  });
+      //add remove to delete
+      trashCan.addEventListener("click", (e) => {
+        let todoItem = e.target.parentElement;
+        //啟動縮小的style，並移除
+        todo.addEventListener("animationend", () => {
+          let text = todoItem.children[0].innerText;
+          let myListArray = JSON.parse(localStorage.getItem("list"));
+          myListArray.forEach((item, index) => {
+            if (item.todoText == text) {
+              myListArray.splice(index, 1);
+              localStorage.setItem("list", JSON.stringify(myListArray));
+            }
+          });
+          todoItem.remove();
+        });
+        //增加縮小的style
+        todo.style.animation = "scaleDown 0.3s forwards";
+      });
+      todo.appendChild(completeButton);
+      todo.appendChild(trashCan);
+
+      section.appendChild(todo);
+    });
+  }
 }
+
+//Merge sort
+function mergeTime(arr1, arr2) {
+  //宣告result把分解開的值push進去
+  let result = [];
+  //宣告arr1 and arr2的第幾個
+  let i = 0;
+  let j = 0;
+  //&&是而且的意思，假如i小於arr1的長度，j小於arr2的長度
+  while (i < arr1.length && j < arr2.length) {
+    //假如
+    if (Number(arr1[i].todoMonth) > Number(arr2[j].todoMonth)) {
+      result.push(arr2[j]);
+      j++;
+    } else if (Number(arr1[i].todoMonth) < Number(arr2[j].todoMonth)) {
+      result.push(arr1[i]);
+      i++;
+    } else if (Number(arr1[i].todoMonth) == Number(arr2[j].todoMonth)) {
+      if (arr1[i].todoDay > arr2[j].todoDay) {
+        result.push(arr2[j]);
+        j++;
+      } else {
+        result.push(arr1[i]);
+        i++;
+      }
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+
+function mergeSort(arr) {
+  //假如arr裡的長度等於1
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    let middle = Math.floor(arr.length / 2);
+    let right = Math.slice(0, middle);
+    let left = Math.slice(middle, arr.length);
+    return mergeTime(mergeSort(right), mergeSort(left));
+  }
+}
+//打印出排列函數中存在localStorage裡的數值
+console.log(mergeSort(JSON.parse(localStorage.getItem("list"))));
+
+let sortButon = document.querySelector(".sort button");
+
+sortButon.addEventListener("click", () => {
+  //sort data排列資料
+  let sortArray = mergeSort(JSON.parse(localStorage.getItem("list")));
+  localStorage.setItem("list", JSON.stringify(sortArray));
+
+  //移出資料 remove data
+  let len = section.children.length;
+  for (let i = 0; i < len; i++) {
+    section.children[0].remove();
+  }
+
+  //lord data
+  lordData();
+});
 //按下enter即可送出
 // let text = document.querySelector("#text");
 // let submit = document.querySelector("#submit");
